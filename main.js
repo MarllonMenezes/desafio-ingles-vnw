46aed5482db447a9f59b85c9088373ecf8f4ecc3
Marllon Menezes <marllon_dallas@hotmail.com>
2022-07-03 20:47:39 -0300

    1º

---
+++ main.js
@@ -0,0 +1,10 @@
+let comedy = {'title': 'Ace Ventura: Pet Detective', 'director': 'Tom Shadyac', 'year': '1994'};
+let action = {'title': 'The Adventures of Sherlock Holmes', 'director': 'Sir Arthur Conan Doyle', 'year': '1892'};
+let fantasy = {'title': 'The Hobbit', 'director': 'Peter Jackson', 'year': '2012'};
+
+if (fantasy != Number) {
+    console.log("MY FAVORIT MOVIE IS...", fantasy.title);
+} else {
+    console.log("NOT EVEN 5 STARS...", comedy.title);
+}
+           
